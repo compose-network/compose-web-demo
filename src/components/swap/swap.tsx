654642df1faf8d8edd817205afca08b640bd23bb
Chris <krzysztof@ssvlabs.io>
2025-10-27 21:58:12 +0100

    temporary debug changes: add placeholder `fromAmount` logic and freeze `values`, remove console.table

--- src/components/swap/swap.tsx
+++ src/components/swap/swap.tsx
@@ -142,7 +142,6 @@ export const Swap: SwapFC = () => {
   // );
 
   const values = form.watch();
-  console.table(values);
 
   useEffect(() => {
     persistPrevSwapValues(values);
@@ -162,7 +161,8 @@ export const Swap: SwapFC = () => {
     {
       tokenIn: getToken(values.fromToken)?.id ?? 0,
       tokenOut: getToken(values.toToken)?.id ?? 0,
-      amountIn: values.fromAmount,
+      // TODO REMOVE!
+      amountIn: parseEther("1"), //values.fromAmount,
     },
     {
       placeholderData: values.fromAmount ? keepPreviousData : undefined,
@@ -190,6 +190,10 @@ export const Swap: SwapFC = () => {
   const submit = form.handleSubmit(async (values) => {
     console.log("values");
     console.table(values);
+
+    // TODO REMOVE!
+    values.fromAmount = parseEther("1");
+    Object.freeze(values);
 
     const [rollupAPublicClient, rollupBPublicClient] =
       createRollupPublicClients(rollupA.id, rollupB.id);
